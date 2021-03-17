5448ac2041e459eeadfb69ac77706b4cfc2ba392
KarthikSundar2002 <karthiksundar30092002@gmail.com>
2021-03-17 10:19:53 +0530

    Add slider for brush size

--- public/sketch2.js
+++ public/sketch2.js
@@ -1,17 +1,21 @@
-
+let strokeW;
+let slider;
 var socket;
 let HOST;
 function setup() {
   createCanvas(windowWidth, 800);
   background(0);
   colorPicker = createColorPicker('#ffffff');
-  colorPicker.position(width/2,height+5);
+  colorPicker.position(width/2,height+10);
+  slider = createSlider(1, 30, 5);
+  slider.position(width/2 - 150, height + 10);
   HOST = location.origin.replace(/^http/, 'ws')
   socket = io.connect(HOST);
 
   socket.on('mouse',(data) => {
       fill(data.color);
       stroke(data.color);
+      strokeWeight(data.strokeWeigh);
       line(data.x, data.y, data.px, data.py);
     }
   );
@@ -25,21 +29,23 @@ function mouseDragged() {
   color1 = colorPicker.value();
   fill(colorPicker.value());
   stroke(colorPicker.value());
-
+  strokeW = slider.value();
+  strokeWeight(strokeW);
   line(mouseX, mouseY, pmouseX, pmouseY);
-  sendmouse(mouseX,mouseY,color1,pmouseX,pmouseY);
+  sendmouse(mouseX,mouseY,color1,pmouseX,pmouseY,strokeW);
 }
 
-function sendmouse(xpos, ypos, color1, pX, pY) {
+function sendmouse(xpos, ypos, color1, pX, pY, strokeW) {
 
-  console.log("sendmouse: " + xpos + " " + ypos);
+
 
   var data = {
     x: xpos,
     y: ypos,
     color: color1,
     px: pX,
-    py: pY
+    py: pY,
+    strokeWeigh: strokeW,
   };
 
 
